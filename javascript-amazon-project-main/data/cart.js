151469a2c7058f61dc2515b22fd1a3469d9156f2
kdcontent <kdcontent18@gmail.com>
2024-05-20 14:37:19 +0900

    Added saveStorage() function to save data to localStorage

--- javascript-amazon-project-main/data/cart.js
+++ javascript-amazon-project-main/data/cart.js
@@ -1,16 +1,26 @@
-export let cart =
-  [
-{
-  productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-  quantity: 2
-}, {
-  productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
-  quantity: 1,
-}, {
-  productId: "19c6a64a-5463-4d45-9af8-e41140a4100c",
-  quantity: 3,
-},
-];
+export let cart = JSON.parse(localStorage.getItem('cart'));
+
+if (!cart) {
+cart = [
+  {
+    productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+    quantity: 2
+  }, {
+    productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+    quantity: 1,
+  }, {
+    productId: "19c6a64a-5463-4d45-9af8-e41140a4100c",
+    quantity: 3,
+  },
+  ];
+
+}
+
+
+
+function saveStorage() {
+  localStorage.setItem('cart', JSON.stringify(cart));
+}
 
 
 export function addToCart(productId) {
@@ -30,6 +40,7 @@ export function addToCart(productId) {
       quantity: 1
        })
   }
+  saveStorage();
 }
 
 
@@ -43,4 +54,5 @@ export function deleteFromCart(productId) {
   })
 
   cart = newCart;
+  saveStorage();
 }
